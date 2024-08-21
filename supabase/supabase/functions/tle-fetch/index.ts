65461fe18b0ba1dcddfd221f26425cc698c03e88
Washio Yusaku <washioyusaku@icloud.com>
2024-08-21 21:41:28 +0900

    supabase: 衛星のステータスを更新するように設定

--- supabase/supabase/functions/tle-fetch/index.ts
+++ supabase/supabase/functions/tle-fetch/index.ts
@@ -33,42 +33,90 @@ Deno.serve(async (req) => {
       satellites,
     });
 
+    // 衛星データを処理するための結果リスト
+    const results = [];
+
     // 各衛星に対して処理ループ
     for (const satellite of satellites) {
-      // 衛星のTLEデータを取得
-      const response = await fetch(
-        `https://celestrak.com/NORAD/elements/gp.php?CATNR=${satellite.norad_id}`
-      );
+      let status = "Error"; // デフォルトのステータス
+      try {
+        // 衛星のTLEデータを取得
+        const response = await fetch(
+          `https://celestrak.com/NORAD/elements/gp.php?CATNR=${satellite.norad_id}`
+        );
 
-      console.log("TLE Fetch Response", {
-        status: response,
-      });
-
-      // TLEテーブルにデータを挿入
-      // id, created_at, norad_id, name, content
-      const {data, error} = await supabase.from("tle").insert([
-        {
-          norad_id: satellite.norad_id,
-          name: satellite.name,
-          content: await response.text(),
-        },
-      ]);
-
-      console.log("Data added", {
-        data,
-      });
-
-      if (error) {
-        return new Response(JSON.stringify({error: error.message}), {
-          headers: {"Content-Type": "application/json"},
-          status: 500,
+        console.log("TLE Fetch Response", {
+          status: response.status,
         });
+
+        // responseがOKの場合
+        if (response.ok) {
+          // response.text()がNo GP data foundの場合
+          const responseText = await response.text();
+          if (responseText.includes("No GP data found")) {
+            status = "Vanished";
+          } else {
+            status = "OK";
+          }
+
+          // satellite_listテーブルを更新
+          const {data: update_data, error: update_error} = await supabase
+            .from("satellite_list")
+            .update({
+              last_updated: new Date(),
+              status: status,
+            })
+            .match({norad_id: satellite.norad_id});
+
+          console.log("Satellite List Updated", {
+            update_data,
+          });
+
+          if (update_error) {
+            results.push({
+              error: update_error.message,
+              satellite: satellite.norad_id,
+            });
+            continue;
+          }
+
+          // tleテーブルにデータを追加
+          const {data, error} = await supabase.from("tle").insert([
+            {
+              norad_id: satellite.norad_id,
+              name: satellite.name,
+              content: responseText,
+            },
+          ]);
+
+          console.log("Data added", {
+            data,
+          });
+
+          if (error) {
+            results.push({error: error.message, satellite: satellite.norad_id});
+          } else {
+            results.push({status: "Success", satellite: satellite.norad_id});
+          }
+        } else {
+          results.push({
+            error: "TLE fetch failed",
+            satellite: satellite.norad_id,
+          });
+        }
+      } catch (error) {
+        console.log("TLE Fetch Error", {
+          error,
+        });
+        results.push({error: error.message, satellite: satellite.norad_id});
       }
     }
 
+    // すべての衛星が処理された後でレスポンスを返す
     return new Response(
       JSON.stringify({
-        message: `tle data added successfully.`,
+        message: `Processing complete.`,
+        results: results,
       }),
       {
         headers: {"Content-Type": "application/json"},
