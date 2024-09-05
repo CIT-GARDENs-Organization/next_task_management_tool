be4639692cb3cc925befa6645380a3d5bbe79df1
Washio Yusaku <washioyusaku@icloud.com>
2024-09-06 00:18:20 +0900

    modify: TLEの型の中でStringであるべきでないものをnumberに変更

--- supabase/supabase/functions/tle-update-webhook/index.ts
+++ supabase/supabase/functions/tle-update-webhook/index.ts
@@ -3,30 +3,30 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 type ParsedTLE = {
   line1: {
     lineNumber: number; // 1 要素データ行番号
-    satelliteCatalogNumber: string; // B 衛星カタログ番号
+    satelliteCatalogNumber: number; // B 衛星カタログ番号
     classification: "S" | "U"; // C 軍事機密種別
-    launchYear: string; // DD 国際衛星識別符号
-    launchNumber: string; // EEE 国際衛星識別符号
+    launchYear: number; // DD 国際衛星識別符号
+    launchNumber: number; // EEE 国際衛星識別符号
     launchPiece: string; // FFF 国際衛星識別符号
-    epochYear: string; // GG 元期(西暦の下2桁)
-    epochDay: string; // HHH.HHHHHHHH 元期（年通算日.その日の00時からの経過時間)
-    meanMotionFirstDerivative: string; // +IIIIIIII (平均運動の1次の時間微分)
-    meanMotionSecondDerivative: string; // +JJJJJ-J (平均運動の2次の時間微分)
-    bStarDragTerm: string; // +KKKKK-K 大気抵抗係数項
-    ephemerisType: number; // L 軌道モデルの種別 0: 情報なし, 1: SGP, 2: SGP4, 3: SDP4, 4: SGP8, 5: SDP8
-    elementSetNumber: string; // MMMM 軌道要素通番
+    epochYear: number; // GG 元期(西暦の下2桁)
+    epochDay: number; // HHH.HHHHHHHH 元期（年通算日.その日の00時からの経過時間)
+    meanMotionFirstDerivative: number; // +IIIIIIII (平均運動の1次の時間微分)
+    meanMotionSecondDerivative: number; // +JJJJJ-J (平均運動の2次の時間微分)
+    bStarDragTerm: number; // +KKKKK-K 大気抵抗係数項
+    ephemerisType: number; // L 軌道モデルの種別
+    elementSetNumber: number; // MMMM 軌道要素通番
     checksum: number; // N チェックサム
   };
   line2: {
     lineNumber: number; // 2 要素データ行番号
-    satelliteCatalogNumber: string; // B 衛星カタログ番号
+    satelliteCatalogNumber: number; // B 衛星カタログ番号
     inclination: number; // PPP.PPPP 軌道傾斜角[度]
     rightAscension: number; // QQQ.QQQQ 昇交点の赤経[度]
     eccentricity: number; // RRRRRRR 離心率
     perigeeArgument: number; // SSS.SSSS 近地点引数[度]
     meanAnomaly: number; // TTT.TTTT 平均近点角[度]
     meanMotion: number; // UU.UUUUUUUU 平均運動[回転/day]
-    revolutionNumber: string; // VVVVV 通算周回数
+    revolutionNumber: number; // VVVVV 通算周回数
     checksum: number; // W チェックサム
   };
 };
@@ -73,30 +73,30 @@ function parseTLE(tle: {line1: string; line2: string}): ParsedTLE {
   return {
     line1: {
       lineNumber: parseInt(line1.slice(0, 1)), // Line 1
-      satelliteCatalogNumber: line1.slice(2, 7).trim(), // BBBBB
+      satelliteCatalogNumber: parseInt(line1.slice(2, 7).trim()), // BBBBB
       classification: line1[7] as "S" | "U", // C
-      launchYear: line1.slice(9, 11).trim(), // DD
-      launchNumber: line1.slice(11, 14).trim(), // EEE
+      launchYear: parseInt(line1.slice(9, 11).trim()), // DD
+      launchNumber: parseInt(line1.slice(11, 14).trim()), // EEE
       launchPiece: line1.slice(14, 17).trim(), // FFF
-      epochYear: line1.slice(18, 20).trim(), // GG
-      epochDay: line1.slice(20, 32).trim(), // HHH.HHHHHHHH
-      meanMotionFirstDerivative: line1.slice(33, 43).trim(), // +IIIIIIII
-      meanMotionSecondDerivative: line1.slice(44, 52).trim(), // +JJJJJ-J
-      bStarDragTerm: line1.slice(53, 61).trim(), // +KKKKK-K
+      epochYear: parseInt(line1.slice(18, 20).trim()), // GG
+      epochDay: parseFloat(line1.slice(20, 32).trim()), // HHH.HHHHHHHH
+      meanMotionFirstDerivative: parseFloat(line1.slice(33, 43).trim()), // +IIIIIIII
+      meanMotionSecondDerivative: parseFloat(line1.slice(44, 52).trim()), // +JJJJJ-J
+      bStarDragTerm: parseFloat(line1.slice(53, 61).trim()), // +KKKKK-K
       ephemerisType: parseInt(line1.slice(62, 63)), // L
-      elementSetNumber: line1.slice(64, 68).trim(), // MMMM
+      elementSetNumber: parseInt(line1.slice(64, 68).trim()), // MMMM
       checksum: parseInt(line1.slice(68, 69)), // N
     },
     line2: {
       lineNumber: parseInt(line2.slice(0, 1)), // Line 2
-      satelliteCatalogNumber: line2.slice(2, 7).trim(), // BBBBB
+      satelliteCatalogNumber: parseInt(line2.slice(2, 7).trim()), // BBBBB
       inclination: parseFloat(line2.slice(8, 16).trim()), // PPP.PPPP
       rightAscension: parseFloat(line2.slice(17, 25).trim()), // QQQ.QQQQ
       eccentricity: parseFloat(`0.${line2.slice(26, 33).trim()}`), // RRRRRRR
       perigeeArgument: parseFloat(line2.slice(34, 42).trim()), // SSS.SSSS
       meanAnomaly: parseFloat(line2.slice(43, 51).trim()), // TTT.TTTT
       meanMotion: parseFloat(line2.slice(52, 63).trim()), // UU.UUUUUUUU
-      revolutionNumber: line2.slice(63, 68).trim(), // VVVVV
+      revolutionNumber: parseInt(line2.slice(63, 68).trim()), // VVVVV
       checksum: parseInt(line2.slice(68, 69)), // W
     },
   };
